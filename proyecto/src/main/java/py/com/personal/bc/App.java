d90a3443e93d0edf747394d855e4518ee9f8b339
Anouk Twijnstra <Anouk.Twijnstra@personal.com.py>
2016-08-29 11:07:28 -0400

    commit con push

--- proyecto/src/main/java/py/com/personal/bc/App.java
+++ proyecto/src/main/java/py/com/personal/bc/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World 22!" );
-		System.out.println( "Desarrollador 2" );
+		System.out.println( "Desarrollador 2 con commit" );
     }
 }
